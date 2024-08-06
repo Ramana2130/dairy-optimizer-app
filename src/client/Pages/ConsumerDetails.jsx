9a82ff7986120f994d773385614d9f7322fdc57b
kabi-60 <kabilashi2005@gmail.com>
2024-08-06 14:54:41 +0530

    consumer

--- src/client/Pages/ConsumerDetails.jsx
+++ src/client/Pages/ConsumerDetails.jsx
@@ -27,14 +27,15 @@ const ConsumerDetails = () => {
     <div className="flex mt-24 items-center justify-center">
 {/* <<<<<<< HEAD */}
       <div className="bg-white flex justify-center border h-[320px] w-96 shadow-2xl relative overflow-hidden hover:shadow-2xl group rounded-xl p-5 transition-all mx-7 duration-500 transform">
-        <div className="flex-row  items-center">
-     <CircleUser     className="w-40 h-40 rounded-full " />
+        <div className="flex-row mx-auto items-center">
+     {/* <CircleUser     className="w-40 h-40 rounded-full mx-auto " /> */}
+     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmCXPYM8NrinZaIE-pjN3pRrVV3UT1t-fnpA&usqp=CAU" className='size-40 rounded-full mx-auto' />
           {/* <div className=""> */}
             <p className="text-gray-800 text-center">Consumer</p>
-            <h1 className="text-black text-center text-2xl font-bold">
+            <h1 className="text-black text-center text-xl font-bold">
               Name:{adminName}
             </h1>
-            <p className="text-xl font-bold text-center text-black transition-opacity duration-500">
+            <p className="text-lg font-bold text-center text-black transition-opacity duration-500">
               Mobile:{adminPhonenumber}123456789
             </p>
           </div>
